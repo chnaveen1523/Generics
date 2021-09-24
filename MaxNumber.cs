ed3536e3c2e3f92aa73bc20973f5a0ab1872ef92
bannu-code-star <nitinchiliveri007@gmail.com>
2021-09-24 09:17:32 +0530

    [Naveen] Added a code

---
+++ MaxNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo.cs
+{
+    class MaxNumber
+    {
+        public static int FindMax(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("two or more numbers are equal");
+                return 0;
+            }
+        }
+
+        public static float FindMax(float first, float second, float third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("two or more numbers are equal");
+                return 0;
+            }
+        }
+    }
+}
---
+++ PrintArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo.cs
+{
+    class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            foreach (int element in intArray)
+            {
+                Console.WriteLine(" " + element + " ");
+            }
+        }
+
+        public static void ToPrint(double[] doubleArray)
+        {
+            foreach (double element in doubleArray)
+            {
+                Console.WriteLine(" " + element + " ");
+            }
+        }
+
+        public static void ToPrint(char[] charArray)
+        {
+            foreach (char element in charArray)
+            {
+                Console.WriteLine(" " + element + " ");
+            }
+        }
+    }
+}
